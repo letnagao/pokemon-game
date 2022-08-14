d0374e9ab73e8b27820cab4d442bee7e6d9a1d15
letnagao <leticia-nagao@hotmail.com>
2022-08-14 03:11:30 -0300

    added battle logic

--- poke.py
+++ poke.py
@@ -12,7 +12,33 @@ class Pokemon:
         return f"{self.name} ({self.primary_type} : {self.hp}/{self.max_hp})" 
 
     def feed(self):
-        self.hp += 1
+        if self.hp < self.max_hp:
+            self.hp += 1
+            print(f"{self.name} has now {self.hp} HP.")
+        else:
+            print(f"{self.name} is full.")
+    
+    def battle(self, other):
+        print("Battle:",self.name, other.name)
+        result = self.typewheel(self.primary_type, other.primary_type)
+        if result == "lose":
+            self.hp -= 10
+            print(f"{self.name} lost and now has {self.hp} HP.")
+        print(f"{self.name} fought {other.name} and the result is a {result}")
+
+    @staticmethod
+    def typewheel(type1,type2):
+        result = {0: "lose", 1: "win", -1: "tie"}
+        game_map = {"water": 0, "fire": 1, "grass": 2}
+
+        wl_matrix = [
+            [-1, 1, 0], 
+            [0, -1, 1],
+            [1, 0, -1],
+        ]
+
+        wl_result = wl_matrix[game_map[type1]][game_map[type2]]
+        return result [wl_result]
 
 if __name__ == '__main__':
     print(Pokemon(name="bulbasaur", primary_type="grass"))
